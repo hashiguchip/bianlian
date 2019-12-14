09f9e24e5975ea360cccb50154bc9b01d9087ed8
hashiguchip <hashiguchip@gmail.com>
2019-12-15 03:04:14 +0900

    🔼 add StopWatch

---
+++ src/core/StopWatch.ts
@@ -0,0 +1,35 @@
+interface ITimerHistory {
+    start: number;
+    stop: number;
+}
+export class StopWatch {
+    static readonly default = 0;
+    private playing = true;
+    private start: number;
+    private history: ITimerHistory[] = [];
+    public play() {
+        if (this.playing) return;
+        this.playing = false;
+        this.start = StopWatch.getNow();
+    }
+    public pause() {
+        if (!this.playing) return;
+        this.playing = false;
+        this.history.push(StopWatch.createHistoryObj(this.start, StopWatch.getNow()));
+    }
+    // todo: add args to options params like a date format
+    public getElapsedTime(): number {
+        if (!this.history) {
+            return StopWatch.default;
+        }
+        return this.history.reduce<number>((accumulation: number, currentValue: ITimerHistory): number => {
+            return accumulation + currentValue.stop - currentValue.start;
+        }, 0);
+    }
+    static getNow(): number {
+        return new Date().getTime();
+    }
+    static createHistoryObj(start: number, stop: number): ITimerHistory {
+        return { start, stop };
+    }
+}
