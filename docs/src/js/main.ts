cc787b35e65d5658724d8a879e4ebbca50cb7151
hashiguchip <hashiguchip@gmail.com>
2019-12-30 22:33:50 +0900

    📘🔼 code highlightjs

--- docs/src/js/main.ts
+++ docs/src/js/main.ts
@@ -1,1 +1,8 @@
 import '../css/index.scss';
+
+import hljs from 'highlight.js';
+import javascript from 'highlight.js/lib/languages/javascript';
+hljs.registerLanguage('javascript', javascript);
+import 'highlight.js/styles/tomorrow-night-eighties.css';
+
+hljs.initHighlightingOnLoad();
