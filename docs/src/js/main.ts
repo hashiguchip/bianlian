ecfa035d7aa1f6ae8ede3f8fe16991e078cd215c
hashiguchip <hashiguchip@gmail.com>
2019-12-31 18:40:14 +0900

    📘🔼 add modal

--- docs/src/js/main.ts
+++ docs/src/js/main.ts
@@ -6,3 +6,28 @@ hljs.registerLanguage('javascript', javascript);
 import 'highlight.js/styles/tomorrow-night-eighties.css';
 
 hljs.initHighlightingOnLoad();
+
+const copyButton = document.querySelector('.js-copy');
+const modalCloseButton = document.querySelector('.js-modal-close');
+const modalWrapper = document.querySelector('.js-modal-wrapper') as HTMLElement;
+const modal = document.querySelector('.js-modal') as HTMLElement;
+
+modal.addEventListener('click', target => {
+    target.stopPropagation();
+});
+
+modalWrapper.addEventListener('click', target => {
+    modalWrapper.classList.remove('-active');
+});
+copyButton.addEventListener('click', target => {
+    console.log(target);
+    modalWrapper.classList.add('-active');
+});
+
+copyButton.addEventListener('click', target => {
+    modalWrapper.classList.add('-active');
+});
+
+modalCloseButton.addEventListener('click', target => {
+    modalWrapper.classList.remove('-active');
+});
