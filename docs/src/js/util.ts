d91f05fcde1a6bb33874d90ab7ab5576558ebcf9
hashiguchip <hashiguchip@gmail.com>
2020-01-01 15:57:04 +0900

    transplant demo to docs directory

--- docs/src/js/util.ts
+++ docs/src/js/util.ts
@@ -1,94 +1,26 @@
-import '../css/index.scss';
-
-import hljs from 'highlight.js';
-import javascript from 'highlight.js/lib/languages/javascript';
-hljs.registerLanguage('javascript', javascript);
-import 'highlight.js/styles/tomorrow-night-eighties.css';
-
-import { SlideAnimation } from '../../../src/core/SlideAnimation';
-
-hljs.initHighlightingOnLoad();
-
-const copyButton = document.querySelector('.js-copy');
-const modalCloseButton = document.querySelector('.js-modal-close');
-const modalWrapper = document.querySelector('.js-modal-wrapper') as HTMLElement;
-const modal = document.querySelector('.js-modal') as HTMLElement;
-const page = document.querySelector('.js-page') as HTMLElement;
-
-modal.addEventListener('click', target => {
-    target.stopPropagation();
-});
-
-modalWrapper.addEventListener('click', target => {
-    page.classList.remove('-blur');
-    modalWrapper.classList.remove('-active');
-    closeModal();
-});
-copyButton.addEventListener('click', target => {
-    page.classList.add('-blur');
-    modalWrapper.classList.add('-active');
-    openModal();
-});
-
-modalCloseButton.addEventListener('click', target => {
-    page.classList.remove('-blur');
-    modalWrapper.classList.remove('-active');
-    closeModal();
-});
-
-let scrollY = 0;
-
-function openModal() {
-    scrollY = window.scrollY;
-    document.body.style.position = 'fixed';
-    document.body.style.top = `-${scrollY}px`;
+declare global {
+    interface Array<T> {
+        deepCopy(): Array<T>;
+    }
 }
-
-function closeModal() {
-    document.body.style.position = '';
-    document.body.style.top = '';
-    window.scrollTo(0, scrollY);
-}
-
-const prev = document.querySelector('[data-prev]');
-const next = document.querySelector('[data-next]');
-const pause = document.querySelector('[data-pause]');
-const block = document.querySelector('[data-block]');
-
-const slideAnimation = new SlideAnimation({
-    element: block as HTMLElement,
-    easing: 'easeInOutCubic',
-    duration: 1000,
-});
-
-const half = function() {
-    slideAnimation.element.style.background = '#9a0014';
+Array.prototype.deepCopy = function() {
+    return this.slice();
 };
 
-const finish = function() {
-    console.log('完了');
-};
-
-prev.addEventListener(
-    'click',
-    () => {
-        slideAnimation.prev({ half, finish });
-    },
-    false
-);
-
-next.addEventListener(
-    'click',
-    () => {
-        slideAnimation.next({ half, finish });
-    },
-    false
-);
-
-pause.addEventListener(
-    'click',
-    () => {
-        slideAnimation.pause();
-    },
-    false
-);
+export class ToggleColors {
+    private readonly master = [];
+    private colors = [];
+    public constructor(args: string[]) {
+        this.master = args;
+        this.colors = this.master.deepCopy();
+    }
+    public getColor() {
+        if (this.master.length === 0) {
+            throw new Error('colors is empty');
+        }
+        const result = this.colors.shift();
+        if (result !== undefined) return result;
+        this.colors = this.master.deepCopy();
+        return this.getColor();
+    }
+}
